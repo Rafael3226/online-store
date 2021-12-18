345a73286829c8a4af778c906a08db3277084ae1
Rafael <rafael.uribe.2080@gmail.com>
2021-12-18 17:14:32 -0500

    Firebase Init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import InputText from './components/forms/InputText'
+
+function App() {
+  return (
+    <div className="container">
+      <InputText />
+    </div>
+  )
+}
+
+export default App
