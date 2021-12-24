19f7ccc226f419bc854c054be475da54f9ad9492
Rafael <rafael.uribe.2080@gmail.com>
2021-12-23 21:27:54 -0500

    Fix env

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,18 +2,18 @@
 import { initializeApp } from '@firebase/app'
 import { getFirestore } from 'firebase/firestore/lite'
 import { getStorage } from 'firebase/storage'
-import { env } from './env'
+//import { env } from './env'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.API_KEY ?? env.API_KEY,
+  apiKey: process.env.API_KEY, //?? env.API_KEY,
   authDomain: 'onlinestore-dd493.firebaseapp.com',
   projectId: 'onlinestore-dd493',
   storageBucket: 'onlinestore-dd493.appspot.com',
-  messagingSenderId: process.env.MESSAGING_SENDER_ID ?? env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID ?? env.APP_ID,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID, //?? env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID, //?? env.APP_ID,
 }
 
 // Initialize Firebase
