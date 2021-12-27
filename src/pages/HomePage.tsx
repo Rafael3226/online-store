bbf78c3d68e57e88c904da7518df2d2c44ce903b
Rafael <rafael.uribe.2080@gmail.com>
2021-12-27 10:08:56 -0500

    Update home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
-import Container from '../containers/Container'
+import Label from '../components/basic/Label'
 import DarkMode from '../containers/DarkMode'
+import ResponsiveCenter from '../containers/ResponsiveCenter'
 import NavBar from '../layout/NavBar'
 
 function HomePage() {
   return (
     <DarkMode>
       <NavBar />
-      <Container>
-        <h6>Home</h6>
-      </Container>
+      <ResponsiveCenter>
+        <Label className="text-4xl">This is the home page</Label>
+        <br />
+        <Label className="my-2">Please go to products</Label>
+      </ResponsiveCenter>
     </DarkMode>
   )
 }
