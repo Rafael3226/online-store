25f6935d7b2ffee07844ecc13948fa5a56cc2df3
Rafael <rafael.uribe.2080@gmail.com>
2021-12-27 18:14:48 -0500

    Fix Deploy Errors

--- src/components/forms/ProductForm.tsx
+++ src/components/forms/ProductForm.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useRecoilState, useSetRecoilState } from 'recoil'
 import FsCollection from '../../db/firestore'
